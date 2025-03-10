7f3d92af0088badde6bf3acc67e150a8f09b5e68
vprabhu1771 <vprabhu1771@gmail.com>
2025-03-10 09:17:49 +0530

    ProfileScreen EditProfilePicScreen added

--- lib/screens/auth/ProfileScreen.dart
+++ lib/screens/auth/ProfileScreen.dart
@@ -5,6 +5,8 @@ import '../HomeScreen.dart';
 
 import '../auth/EditProfileScreen.dart';
 
+import 'EditProfilePicScreen.dart';
+
 class ProfileScreen extends StatefulWidget {
   final String title;
 
@@ -53,12 +55,47 @@ class _ProfileScreenState extends State<ProfileScreen> {
           padding: const EdgeInsets.all(16.0),
           child: Column(
             children: [
+
               // Profile Image View
               Center(
-                child: CircleAvatar(
-                  radius: 50,
-                  backgroundImage: NetworkImage('https://gravatar.com/avatar/${user!.email}'), // Replace with the user's image URL
-                  backgroundColor: Colors.grey[200],
+                child: Stack(
+                  children: [
+                    CircleAvatar(
+                    radius: 50,
+                    backgroundImage: NetworkImage(
+                        user?.userMetadata?['image_path'] ?? 'https://gravatar.com/avatar/${user!.email}'),
+                    backgroundColor: Colors.grey[200],
+                  ),
+                  Positioned(
+                    bottom: 0,
+                    right: 0,
+                    child: GestureDetector(
+                      onTap: () async {
+
+                        print(user?.userMetadata?['image_path']);
+
+                        bool? result = await Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => EditProfilePicScreen(),
+                          ),
+                        );
+
+                        if (result == true) {
+                          // Refresh data after edit
+                          // Refresh UI
+                          await refreshUserData();
+                        }
+
+                      },
+                      child: CircleAvatar(
+                        radius: 18,
+                        backgroundColor: Colors.blue,
+                        child: Icon(Icons.edit, color: Colors.white, size: 18),
+                      ),
+                    ),
+                  ),
+                  ],
                 ),
               ),
               const SizedBox(height: 16),
@@ -81,7 +118,7 @@ class _ProfileScreenState extends State<ProfileScreen> {
                   if (result == true) {
                     await refreshUserData(); // Refresh data after edit
                   }
-                  
+
                 },
               ),
               const Divider(),
