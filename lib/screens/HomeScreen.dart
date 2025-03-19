1826d9310c2c07e47f52b7245d0f781cd2b3fd81
vprabhu1771 <vprabhu1771@gmail.com>
2025-03-19 18:42:06 +0530

    Update HomeScreen.dart

--- lib/screens/HomeScreen.dart
+++ lib/screens/HomeScreen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_supabase_chat/widgets/CustomDrawer.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
+import 'ChatDetailScreen.dart';
 import 'NewChatScreen.dart';
 
 
@@ -15,6 +17,26 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  final supabase = Supabase.instance.client;
+  List<Map<String, dynamic>> _conversations = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchConversations();
+  }
+
+  Future<void> fetchConversations() async {
+    try {
+      final response = await supabase.from('conversations').select();
+      setState(() {
+        _conversations = response;
+      });
+    } catch (error) {
+      print("Error fetching conversations: $error");
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -22,8 +44,30 @@ class _HomeScreenState extends State<HomeScreen> {
         title: Text(widget.title),
       ),
       drawer: CustomDrawer(parentContext: context),
-      body: Center(
-        child: Text(widget.title),
+      body: _conversations.isEmpty
+          ? Center(child: CircularProgressIndicator())
+          : ListView.builder(
+        itemCount: _conversations.length,
+        itemBuilder: (context, index) {
+          final conversation = _conversations[index];
+          return ListTile(
+            title: Text(conversation['name'] ?? 'Unnamed Chat'),
+            subtitle:
+            Text(conversation['is_group'] ? 'Group Chat' : 'Private Chat'),
+            onTap: () {
+              // Handle chat navigation
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => ChatDetailScreen(
+                    conversationId: conversation["id"],
+                    receiverEmail: "receiverEmail",
+                  ),
+                ),
+              );
+            },
+          );
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
